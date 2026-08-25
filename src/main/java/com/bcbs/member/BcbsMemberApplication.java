ad063e766fd4fee9eb60ec6296310fcd3197c201
Tej K <tejk@Tejaswis-MacBook-Air.local>
2026-08-25 14:51:05 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/bcbs/member/BcbsMemberApplication.java
@@ -0,0 +1,11 @@
+package com.bcbs.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcbsMemberApplication{
+    public static void main(String[] args){
+        SpringApplication.run(BcbsMemberApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bcbs/member/BcbsMemberApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bcbs.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BcbsMemberApplicationTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
